18bb4ac5677884b2e39fa8ddd2609a38e3bf1ec9
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-23 23:16:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, data|
     data.each do |holiday, supplies|
-      puts "#{season}:"
+      puts "#{season.to_s.capitalize}:"
       puts "#{holiday.to_s.capitalize}: #{supplies.join(', ')}"
     end
   end
