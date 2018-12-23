c3cabd9a9eefd5d08e3f7cb75b7c32469b06d8cd
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-23 23:35:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,9 +60,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, data|
     data.each do |holiday, supplies|
-      formattedHoliday = holiday.sub!('_',' ').to_s.split.capitalize.join(' ')
+      formattedHoliday = holiday.sub!('_',' ').to_s.split.capitalize!.join(' ')
       puts "#{season.to_s.sub('_',' ').capitalize}:"
-      puts "  #{holiday.to_s.capitalize}: #{supplies.join(', ')}"
+      puts "  #{formattedHoliday}: #{supplies.join(', ')}"
     end
   end
 end
