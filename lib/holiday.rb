cccf03df64fcac665f3cf851c4d7a8d63e7f2bf1
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-23 23:13:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,5 +70,6 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash[season].select do |holiday|
-    holiday.include?
+    holiday.include?("BBQ")
+  end
 end
